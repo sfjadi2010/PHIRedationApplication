0d87e6567b53933dff65f1c0eca63be6aa8f609c
sfjadi2010 <sfjadi2010@live.com>
2025-02-16 14:08:34 -0500

    Initialize Solution

---
+++ phiredationapplication.client/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+interface WeatherForecast {
+  date: string;
+  temperatureC: number;
+  temperatureF: number;
+  summary: string;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+  imports: [CommonModule]
+})
+export class AppComponent {
+
+  constructor() {}
+
+}
